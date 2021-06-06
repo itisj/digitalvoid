05f6eb88ba82d443190563e8a58e95bc6969676e
jerrydgj <jerrydgj@users.noreply.github.com>
2021-06-07 03:33:13 +1000

    digital void first commit

---
+++ this.js
@@ -0,0 +1,12 @@
+
+// play music
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+// dark mode
+function darkFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
